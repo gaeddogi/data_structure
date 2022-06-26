516891a4f28124df7d60f755485a3f79fba4af3f
gaeddogi <dlgkdud7845@naver.com>
2022-06-26 18:59:00 +0900

    test commit

--- src/Heap/Main.java
+++ src/Heap/Main.java
@@ -29,6 +29,8 @@ public class Main {
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
 
+        //test
+
         //hashCode()는 주소값을 기반으로 다이제스트를 생성하는데
         //기본 자료형(Primitive Type) String, Integer, Double..... 등은 내부적으로
         //hashCode()를 오버라이드 하여 내용이 같으면 같은 다이제스트를 반환하도록 구현되어있다.
