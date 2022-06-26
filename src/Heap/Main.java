0541f7e758334ac821979935ad8d9a23ef172bfa
gaeddogi <dlgkdud7845@naver.com>
2022-06-26 18:58:20 +0900

    test commit

---
+++ src/Heap/Main.java
@@ -0,0 +1,38 @@
+package Heap;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+//        Heap_미완성 heap = new Heap_미완성();
+//        Random rnd = new Random();
+//
+//        for (int i = 0; i < 5; i++) {
+//            int a = rnd.nextInt(100);
+//            System.out.println(a);
+//            heap.add(a);
+//        }
+//
+//        System.out.println(heap);
+//        heap.add(2);
+//        System.out.println(heap);
+//
+//        heap.add(89);
+//        System.out.println(heap);
+//
+//        System.out.println(heap.remove());
+//        System.out.println(heap);
+
+        String a = "안녕";
+        String b = "안녕";
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+        //hashCode()는 주소값을 기반으로 다이제스트를 생성하는데
+        //기본 자료형(Primitive Type) String, Integer, Double..... 등은 내부적으로
+        //hashCode()를 오버라이드 하여 내용이 같으면 같은 다이제스트를 반환하도록 구현되어있다.
+
+
+    }
+}
